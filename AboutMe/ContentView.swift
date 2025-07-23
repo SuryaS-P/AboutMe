2d04d9526f366fec2bb846bf49b01783840c07d9
Surya Singh-Prasad <surya.singhprasad@gmail.com>
2025-07-23 14:01:08 -0400

    Wrote main text and added books image.

--- AboutMe/ContentView.swift
+++ AboutMe/ContentView.swift
@@ -21,7 +21,27 @@ struct ContentView: View {
                     .foregroundColor(Color.gray)
                 Image("FountainPen")
                     .cornerRadius(2100)
-                
+                Text("BOOKS & POETRY")
+                    .font(.title2)
+                    .fontWeight(.heavy)
+                    .foregroundColor(Color(hue: 0.63, saturation: 0.28, brightness: 0.68))
+                    .padding(10.0)
+                HStack{
+                    Text("Books are one of the most important things in my life. I am a voracious reader, and I don't have a particular favourite genre, though I am fond of primarily fiction.")
+                        .font(.body)
+                        .fontWeight(.light)
+                        .multilineTextAlignment(.center)
+                        .padding(15.0)
+                        
+                    Text("Poetry is probably the most important part of my life, and I frequently write it, mostly free verse, though I have experimented in poetic styles. One of my favourites is a rimas dissolutas.")
+                        .font(.body)
+                        .fontWeight(.light)
+                        .multilineTextAlignment(.center)
+                        .padding(15.0)
+                }
+                Image("OldBlueBooks")
+                    .cornerRadius(50)
+                    
             }
         }
     }
